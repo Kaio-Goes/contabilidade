9f4546b721086f85585d0ab6d1218da6205e4801
Kaio <kaiogoes1999@gmail.com>
2021-09-07 14:04:05 -0300

    comentando o link inscreva-se

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -12,14 +12,14 @@ const LoginPage: React.FC = () => {
                 <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                     <div className="text-center mt-24">
                         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Login</h2>
-                        <p className="mt-2 text-center text-md text-gray-600">
+                        {/* <p className="mt-2 text-center text-md text-gray-600">
                             {"Não tem uma conta?"}
                             <Link href="signup">
                                 <a href="#" className="text-blue-500">
                                     Inscrever-se
                             </a>
                             </Link>
-                        </p>
+                        </p> */}
                     </div>
                     <div className="mt-8 bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
                         <LoginForm />
