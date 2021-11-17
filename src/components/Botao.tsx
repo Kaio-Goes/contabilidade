4abae26f3bcba7b4ed3547011ffc40bfc04f42e1
Kaio <kaiogoes1999@gmail.com>
2021-11-17 11:40:36 -0300

    tirando a 3 tentativa

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -10,7 +10,8 @@ export default function Botao(props: BotaoProps) {
     const cor = props.cor
     return (
         <button onClick={props.onClick} className={`
-            bg-${cor}-700
+            bg-${`${cor}`}-600
+            text-white px-4 py-2 rounded-md
             ${props.className}
         `}>
             {props.children}
